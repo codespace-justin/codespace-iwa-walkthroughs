1e153d3c3f2236fd2482a57c39f4d851951acdac
Justin Scott Jenecke <93656319+codespace-justin@users.noreply.github.com>
2022-04-05 09:27:43 +0200

    creates 2nd inheritence excercise

--- 9-inheritence/lang-inheritence/src/app.js
+++ 9-inheritence/lang-inheritence/src/app.js
@@ -1,7 +1,7 @@
 
 // import class using Node Modules - import/export syntax
-import { Framework } from "./classes/Framework.js";
-import { Language } from "./classes/Language.js";
+import { Framework } from "./classes/Framework.js"; // import framework class
+import { Language } from "./classes/Language.js"; // import language class
 
 
 /* ---------- Instantiate Objects from classes: ---------- */
@@ -34,32 +34,6 @@ console.log('\n');
 console.log(frame1);
 console.log('\n');
 
-
-/* ----------------- Property Testing ----------------- */
-
-// --- Language --- //
-
-// test setters - change Java to Python
-'Language Setters:'
-lang1.changeId = 1 
-lang1.changeName = "Python" 
-lang1.changeUses = ["Machine Learning", "Data Science", "Data Analysis"]
-lang1.changeDescription = "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation"
-
-console.log(lang1.changeName = "Swift");
-
-
-// --- Framework --- //
-
-
-// test setters
-'\nFramework Setters:'
-frame1.changeId = 101 
-frame1.changeName = "Vue.js" 
-frame1.changeUses = ["Web Development", "Bulding Frontends", "Bulding Single-Page Applications"]
-frame1.changeDescription = `Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications. It was created by Evan You, and is maintained by him and the rest of the active core team members.`
-frame1.changeSet = "JavaScript"
-frame1.changeStack = "Frontend"
 
 
 
